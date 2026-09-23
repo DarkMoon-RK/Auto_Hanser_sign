15351e2c7eeef4e700a6002667ae9b8624c75b96
DarkMoon-RK <1836807696@qq.com>
2026-09-23 09:47:47 +0800

    Modify ChromeOptions for headless mode and window size
    Updated ChromeOptions to use 'headless=new' and set window size.

--- main.py
+++ main.py
@@ -44,11 +44,12 @@ def send_notification(subject, body):
 
 # 初始化 WebDriver
 options = webdriver.ChromeOptions()
-options.add_argument('--headless') 
+options.add_argument('--headless=new') 
 options.add_argument('--no-sandbox')
 options.add_argument('--disable-dev-shm-usage')
 options.add_argument('--ignore-certificate-errors')
 options.add_argument('--disable-gpu')
+options.add_argument('--window-size=1920,1080')
 
 driver = webdriver.Chrome(options=options)
 url = "https://2550505.com/" 
